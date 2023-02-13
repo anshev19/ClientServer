689fe8b13683117eab0c01b45aafd849aae001be
aleksandr.shevchenko <aleksandr.shevchenko@nexign.com>
2023-02-13 14:58:13 +0300

    Client and server dialog

---
+++ src/main/java/Client.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    public static void main(String[] args) {
+        String host = "netology.homework";
+        int port = 8080;
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println(in.readLine());
+            String resp = reader.readLine();
+            out.println(resp);
+            System.out.println(in.readLine());
+            out.println(reader.readLine());
+            System.out.println(in.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
